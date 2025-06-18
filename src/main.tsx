7b659d59247d56bfb6224895b2390125f805c9ed
noql1 <adil.saktagan@nu.edu.kz>
2025-06-18 19:26:20 +0500

    normalize added

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { CssBaseline } from '@mui/material';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
+          <CssBaseline />
           <App />
       </QueryClientProvider>
 )
